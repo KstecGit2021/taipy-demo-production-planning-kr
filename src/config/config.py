3a4da64c0080e4b050b10c25bc6976e8741fcfe1
KSTEC_dsna <78070883+KstecGit2021@users.noreply.github.com>
2022-09-05 13:49:48 +0900

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -5,16 +5,16 @@ from taipy.core import Frequency
 import json
 from algos.algos import *
 
-# This code is the one to produce scenario_cfg and pipeline_csg. These two variables are used to create new scenarios
-# in the main code
+# 이 코드는 시나리오_cfg 및 파이프라인_csg를 생성하는 코드입니다. 
+# 이 두 변수는 기본 코드에서 새 시나리오를 만드는 데 사용됩니다.
 
-# this code is what will configure our graph of execution
+# 이 코드는 실행 그래프를 구성할 것입니다.
 
 ###############################################################################
-# Datanode
+# 데이터노드
 ###############################################################################
 
-# we create our first datanode, the source is csv file
+# 첫 번째 데이터 노드를 생성합니다. 소스는 csv 파일입니다.
 path_to_demand = 'data/time_series_demand.csv'
 demand_cfg = Config.configure_data_node(id="demand",
                                 storage_type="csv", 
@@ -26,19 +26,19 @@ demand_cfg = Config.configure_data_node(id="demand",
 fixed_variables_default_json = open('data/fixed_variables_default.json')
 fixed_variables_default = json.load(fixed_variables_default_json)
 
-# creation of our second datanode that will have as a default data our fixed_variables_default
-# this is this datanode that we will write on when we submit other values for fixed_variable
+# fixed_variables_default를 기본 데이터로 하는 두 번째 데이터 노드 생성 
+# 이것은 fixed_variable에 대한 다른 값을 제출할 때 쓸 이 데이터 노드입니다.
 fixed_variables_cfg = Config.configure_data_node(id="fixed_variables", default_data = fixed_variables_default, scope=Scope.PIPELINE)
 
-# here are the datanodes that keep track of the model : the model_created datanode, the model_solved datanode
+# 모델을 추적하는 데이터 노드는 다음과 같습니다. model_created 데이터 노드, model_solved 데이터 노드
 model_created_cfg = Config.configure_data_node(id="model_created", scope=Scope.PIPELINE)
 model_solved_cfg = Config.configure_data_node(id="model_solved", scope=Scope.PIPELINE)
 
-# and this is the datanode that will be used to get our results from the main code
+# 이것은 메인 코드에서 결과를 얻는 데 사용할 데이터 노드입니다.
 results_cfg = Config.configure_data_node(id="results", scope=Scope.PIPELINE)
 
 ###############################################################################
-# Task
+# 작업
 ###############################################################################
 
 # (demand_cfg,fixed_variables_cfg) -> |create_model| -> (model_created_cfg)
@@ -61,13 +61,13 @@ create_results_cfg = Config.configure_task(id="create_results",
 
 
 ###############################################################################
-# Pipeline and scenario config
+# 파이프라인 및 시나리오 구성
 ###############################################################################
 
-# Pipeline : execution of tasks. It refers to the execution of a serie of tasks 
+# 파이프라인 : 작업 실행. 일련의 작업을 수행하는 것을 말합니다. 
 pipeline_cfg = Config.configure_pipeline(id="pipeline",task_configs=[create_model_task,solve_model_cfg,create_results_cfg])
 
-# scenario : execution of pipeline. It refers to the execution of a serie of pipelines (here just one)
-# this is through this variable that we will create new scenarios
+# 시나리오 : 파이프라인 실행. 일련의 파이프라인(여기서는 하나만)의 실행을 나타냅니다.
+# 이 변수를 통해 새로운 시나리오를 생성합니다.
 scenario_cfg = Config.configure_scenario(id="scenario",pipeline_configs=[pipeline_cfg], frequency=Frequency.MONTHLY)
 
