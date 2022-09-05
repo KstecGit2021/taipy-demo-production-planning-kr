6129c8828082dc72e52936d93cbd1bdc58166d4f
KSTEC_dsna <78070883+KstecGit2021@users.noreply.github.com>
2022-09-05 14:55:37 +0900

    Update data_visualization_md.py

--- src/pages/data_visualization_md.py
+++ src/pages/data_visualization_md.py
@@ -4,7 +4,7 @@ import json
 with open('data/fixed_variables_default.json', "r") as f:
     fixed_variables_default = json.load(f)
 
-# no code from Taipy Core has been executed yet, we read the csv file this way
+# Taipy Core의 코드는 아직 실행되지 않았습니다. csv 파일을 이런 식으로 읽습니다.
 da_initial_demand = pd.read_csv('data/time_series_demand.csv')
 da_initial_demand = da_initial_demand[['Year', 'Month', 'Demand_A', 'Demand_B']].astype(int)
 
@@ -14,7 +14,7 @@ da_initial_demand.columns = [col.replace('_', ' ')
 da_initial_variables = pd.DataFrame({key: [fixed_variables_default[key]]
                                     for key in fixed_variables_default.keys() if 'Initial' in key})
 
-# The code below is to correctly format the name of the columns
+# 아래 코드는 열 이름의 형식을 올바르게 지정하는 것입니다.
 da_initial_variables.columns = [col.replace('_', ' ').replace('one', '1').replace('two', '2').replace('initial ', '')
                                 for col in da_initial_variables.columns]
 da_initial_variables.columns = [col[0].upper() +
@@ -22,13 +22,13 @@ da_initial_variables.columns = [col[0].upper() +
 
 
 da_data_visualisation_md = """
-# Data Visualization
+# 데이터 시각화
 
 <|Expand here|expanded=False|expandable|
 
 <|layout|columns=1 1 1|columns[mobile]=1|
 <|
-## Initial stock
+## 초기 재고
 
 <center>
 <|{da_initial_variables[[col for col in da_initial_variables.columns if 'Stock' in col]]}|table|show_all|width=445px|>
@@ -36,7 +36,7 @@ da_data_visualisation_md = """
 |>
 
 <|
-## Initial Production
+## 초기 생산
 
 <center>
 <|{da_initial_variables[[col for col in da_initial_variables.columns if 'Production' in col]]}|table|show_all|width=445px|>
@@ -44,7 +44,7 @@ da_data_visualisation_md = """
 |>
 
 <|
-## Incoming Purchased Material
+## 구매한 자료
 
 <center>
 <|{da_initial_variables[[col for col in da_initial_variables.columns if 'Purchase' in col]]}|table|show_all|width=445px|>
@@ -53,14 +53,14 @@ da_data_visualisation_md = """
 |>
 
 
-## Demand of the upcoming months
+## 다가오는 달의 수요
 
 <center>
 <|{da_initial_demand.round()}|table|width=fit-content|show_all|height=fit-content|>
 </center>
 |>
 
-## Evolution of the demand
+## 수요의 진화
 
 <|{da_initial_demand}|chart|x=Month|y[1]=Demand A|y[2]=Demand B|width=100%|>
 """
